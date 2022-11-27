6aa5cff5f4066911302bd07160d450ee604b14af
Ludicrous_peridot <>
2022-11-28 00:41:49 +0300

    Keeping debug printouts in for now. Can now walk and smash things.

--- P_TICK.C
+++ P_TICK.C
@@ -90,11 +90,17 @@ static void RunThinkers(void)
 		{ // Time to remove it
 			currentthinker->next->prev = currentthinker->prev;
 			currentthinker->prev->next = currentthinker->next;
-			Z_Free(currentthinker);
+//			Z_Free(currentthinker);
 		}
 		else if(currentthinker->function)
 		{
+#if defined(DEBUG_PTRS)
+			dmprintf("+function %p(%p)\n", currentthinker->function, currentthinker);
+#endif			
 			currentthinker->function(currentthinker);
+#if defined(DEBUG_PTRS)
+			dmprintf("-function %p\n", currentthinker->function);
+#endif			
 		}
 		currentthinker = currentthinker->next;
 	}
